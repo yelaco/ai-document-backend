67c32d55ed6f8313699cb2f71354ce49f7768b7a
Quang M. Bui <minhquangbui053@gmail.com>
2025-10-04 10:55:04 +0700

    refactor(app_module): refactor

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,12 +5,16 @@ import { LoggingInterceptor } from './shared/interceptors/logging.interceptor';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { Document } from './documents/entities/document.entity';
+import { UsersModule } from './users/users.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
     ConfigModule,
-    DocumentsModule,
     AiModule,
+    DocumentsModule,
+    UsersModule,
+    AuthModule,
     TypeOrmModule.forRootAsync({
       imports: [ConfigModule],
       inject: [ConfigService],
